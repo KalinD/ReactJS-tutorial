654cfdc90961746fc754a85ecd95085b31bc7643
KalinD <doiychevk@gmail.com>
2021-07-17 19:35:50 +0300

    Sidebar opens and closes

--- website/src/components/AdminWrapper.js
+++ website/src/components/AdminWrapper.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import "./assets/css/admin.css";
 
 import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
+import { duration, withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -13,6 +13,8 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Divider from '@material-ui/core/Divider';
 
 const drawerWidth = 240;
 const styles = theme => ({
@@ -20,24 +22,70 @@ const styles = theme => ({
         paddingRight: 24
     },
     appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        })
+    },
+    appBarShift: {
         marginLeft: drawerWidth,
-        width: `clacl(100% - ${drawerWidth}px)`
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        })
     },
     drawerPaper: {
         position: 'relative',
         whiteSPace: 'noWrap',
-        width: drawerWidth
+        width: drawerWidth,
+        transition: theme.transitions.create(['width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        })
+    },
+    drawerPaperClose: {
+        overflowX: 'hidden',
+        width: theme.spacing.unit * 7,
+        transition: theme.transitions.create(['width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        })
+    },
+    toolbarIcon: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar
     }
 });
 
 class AdminWrapper extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            open: true
+        }
+    }
+
+    handleDrawerOpen = (e) => {
+        this.setState({ open: true });
+    }
+
+    handleDrawerClose = (e) => {
+        this.setState({ open: false });
+    }
+
     render() {
         const { classes } = this.props;
         return (
             <div id="admin-page">
-                <AppBar className={ classes.appBar}>
+                <AppBar className={classNames(classes.appBar, this.state.open && classes.appBarShift)}>
                     <Toolbar className={classes.toolbar}>
-                        <IconButton>
+                        <IconButton onClick={this.handleDrawerOpen}>
                             <MenuIcon />
                         </IconButton>
                         <Typography
@@ -49,10 +97,16 @@ class AdminWrapper extends Component {
                     </Toolbar>
                 </AppBar>
                 <Drawer
-                    classes={{ paper: classes.drawerPaper }}
+                    classes={{ paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose) }}
                     variant="permanent"
                     open={true}
                 >
+                    <div className={classes.toolbarIcon}>
+                        <IconButton onClick={this.handleDrawerClose}>
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </div>
+                    <Divider />
                     <List>
                         <ListItem>Dasboard</ListItem>
                     </List>
